b33bd8aac07e5a8899e7c31776ccfd93cfbc4c75
Akash-Edake <akashedake30@gmail.com>
2023-09-25 08:57:03 +0530

    new project

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
+  server: { //for run server 3000 port
+    port: 3000  
   }
 })
