16f534415f7578da7bd0a0c3fac80fd460f41505
Akash-Edake <akashedake30@gmail.com>
2023-11-10 20:53:42 +0530

    lazy loading

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -1,21 +1,26 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import React from "../components/pages/React/React";
-import Html from "../components/pages/Html/Html";
-import JavaScript from "../components/pages/JavaScript/JavaScript";
-import Css from "../components/pages/Css/Css";
-import Angular from "../components/pages/Angular/Angular";
+import ReactComp from "../components/pages/React/React";
+const Html = React.lazy(() => import("../components/pages/Html/Html"));
+const JavaScript = React.lazy(() =>
+  import("../components/pages/JavaScript/JavaScript")
+);
+const Css = React.lazy(() => import("../components/pages/Css/Css"));
+const Angular = React.lazy(() => import("../components/pages/Angular/Angular"));
 
 function Routing() {
   return (
     <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<React />} />
-        <Route path="/react" element={<React />} />
+        <Route path="/" element={<ReactComp />} />
+        <Route path="/react" element={<ReactComp />} />
         <Route path="/angular" element={<Angular />} />
         <Route path="/html" element={<Html />} />
         <Route path="/javaScript" element={<JavaScript />} />
         <Route path="/css" element={<Css />} />
       </Routes>
+      </Suspense>
     </>
   );
 }
