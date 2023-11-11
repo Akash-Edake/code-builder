b97a2c1aa51aa0d90c279a6998c262f9be3bc1cc
Akash-Edake <akashedake30@gmail.com>
2023-11-11 16:19:13 +0530

    ercel Analytics

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import ReactComp from "../components/pages/React/React";
 const Html = React.lazy(() => import("../components/pages/Html/Html"));
 const JavaScript = React.lazy(() =>
@@ -11,16 +12,17 @@ const Angular = React.lazy(() => import("../components/pages/Angular/Angular"));
 function Routing() {
   return (
     <>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<ReactComp />} />
-        <Route path="/react" element={<ReactComp />} />
-        <Route path="/angular" element={<Angular />} />
-        <Route path="/html" element={<Html />} />
-        <Route path="/javaScript" element={<JavaScript />} />
-        <Route path="/css" element={<Css />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<ReactComp />} />
+          <Route path="/react" element={<ReactComp />} />
+          <Route path="/angular" element={<Angular />} />
+          <Route path="/html" element={<Html />} />
+          <Route path="/javaScript" element={<JavaScript />} />
+          <Route path="/css" element={<Css />} />
+        </Routes>
       </Suspense>
+      <Analytics debug={false} />
     </>
   );
 }
