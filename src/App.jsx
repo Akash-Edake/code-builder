4513f4bdb745baf9a15138fe9e85e532a2584298
Akash-Edake <akashedake30@gmail.com>
2023-11-15 20:34:32 +0530

    signIn any path

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/signUp" element={<SignUp />} />
+          <Route path="*" element={<SignIn />} />
         </Routes>
         </>
       )}
